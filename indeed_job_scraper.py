4b063cd93e0d45f4016de217334c38bc1a59f550
Jyoti <jyoti.bartaula@gmail.com>
2019-03-27 21:45:45 +0200

    add function to read file containing skill set to count

--- indeed_job_scraper.py
+++ indeed_job_scraper.py
@@ -10,10 +10,11 @@ import sys
 #nltk.download()
 nltk.data.path.append('./nltk_data/')  # set the path
 
+
 def get_job_link(job_search_url):
     """
     Given the url for specific job search, this function returns the
-     url for all the open position for job search
+     url for all the open position for that job
 
       Returns:
         urls: a python list of all job posting links
@@ -33,9 +34,10 @@ def get_job_link(job_search_url):
                 partial_url = link.a.get('href')   # extract href i.e link to individual job posting
                 complete_url.append( main_url+partial_url)
 
-        if number_of_pages > 1:   # if there are more than one pages if job advertising
+        if number_of_pages > 1:   # if there are more than one pages of job advertising
             start = 10
-            for i in range(2, number_of_pages+1):
+            # we start from page  2 as for page 1 we have already dealt above
+            for i in range(2, number_of_pages+1): # we start from page
                 #print("https://www.indeed.fi/jobs?q=python+&l=Tampere&start="+str(start))
                 source = requests.get(job_search_url+str(start)).text
                 soup = BeautifulSoup(source, "lxml")
@@ -46,17 +48,17 @@ def get_job_link(job_search_url):
                 start = 10*i
         return complete_url
     except:
-        print("Something wrong with URL. Please check again")
+        print("Something wrong with URL. Please check Your input and try again.")
         sys.exit()
 
 
 
 def count_words(job_urls):
     """
-    Given the list of links to all the job, this function returns the number of occurance of
-    key job skill (skill count) in those vacancy.
+    Given the list of urls to all open position, this function returns the number of occurance of
+    key job skill (skill count) in those vacancies.
     """
-    word_counter = Counter()  # declaring an empty countet
+    word_counter = Counter()  # declaring an empty counter
     for url in job_urls:
         source_each_job = requests.get(url).text
         soup_each_job = BeautifulSoup(source_each_job, "lxml")
@@ -71,19 +73,29 @@ def count_words(job_urls):
         # remove the stop words
         final_words = [word for word in raw_words if word not in stop_words_english]
         word_counter.update(final_words)  # updating the counter with list of new final words
-    skill_counter = Counter({"Angular":word_counter["angular"], "AWS":word_counter["aws"], "JavaScript":word_counter["javascript"],
+
+    skill_to_count = []
+    skill_to_count = read_file() ## call the function to read the file containing words to count
+    skill_counter = {}
+    if skill_to_count:
+        for word in skill_to_count:
+           skill_counter[word] = word_counter[word]
+
+        return skill_counter
+    else:
+        skill_counter = Counter({"Angular":word_counter["angular"], "AWS":word_counter["aws"], "JavaScript":word_counter["javascript"],
                         "C++":word_counter["c++"],"HTML":word_counter["html5"] ,"CSS":word_counter["css"],
                         "Azure":word_counter["azure"], "Java":word_counter["java"], "Scala":word_counter["scala"],
                         "Python":word_counter["python"], "Node.js":word_counter["node.js"],"React":word_counter["react"],
                         "Ruby":word_counter["ruby"], "R":word_counter["r"],"Postgres":word_counter["postgresql"],
                         "C":word_counter["c"], "angualar.js":word_counter["angular.js"]} )
-    return dict(skill_counter)
+        return dict(skill_counter)
 
 
 def generate_url():
     """
-        this function takes the job title and location as user request and
-        return the indeed url to search for all the open position for given job.
+        this function takes the job title and location as user input and
+        return the main indeed job url.
 
         returns : job search url as string
     """
@@ -93,6 +105,28 @@ def generate_url():
     query_param_job = '+'.join(job_title)
     job_search_url = 'https://www.indeed.fi/jobs?q={}&l={}'.format(query_param_job, location)
     return job_search_url
+
+def read_file():
+    """
+      This function reads the text file that contains job skills , whose occurance
+      will be counted in job vacancies.
+    """
+    words_to_count = []
+    try:
+        with open('words_to_counts.txt', 'r') as f:
+            for line in f:
+                line = line.strip().split(',')
+                for word in line:
+                    if word == "":
+                        continue
+                    words_to_count.append(word.strip().lower())
+        return words_to_count
+    except Exception:
+        print("The file containing the job skill was not found.")
+        print("Programe will return default skill counts.")
+        print('\n')
+
+
 
 if __name__ == "__main__":
     job_search_url = generate_url()
